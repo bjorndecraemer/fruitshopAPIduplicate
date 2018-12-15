b58a5686bbf8283f61baa8a8efb1cdd22b770e77
bjorn <bjorn.decraemer@yahoo.com>
2018-12-15 23:28:24 +0100

    Added Customer customer_url

--- src/main/java/bjorn/petprojects/fruitshopduplicate/api/v1/model/CustomerDTO.java
+++ src/main/java/bjorn/petprojects/fruitshopduplicate/api/v1/model/CustomerDTO.java
@@ -1,10 +1,15 @@
 package bjorn.petprojects.fruitshopduplicate.api.v1.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDTO {
     private Long id;
     private String firstname;
     private String lastname;
+    private String customerURL;
 }
--- src/main/java/bjorn/petprojects/fruitshopduplicate/services/CustomerServiceImpl.java
+++ src/main/java/bjorn/petprojects/fruitshopduplicate/services/CustomerServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    public static final String API_V_1_CUSTOMERS = "/api/v1/customers/";
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
 
@@ -24,21 +25,28 @@ public class CustomerServiceImpl implements CustomerService {
     public List<CustomerDTO> findAllCustomers() {
         return customerRepository.findAll()
                 .stream()
-                .map(customerMapper::customerToCustomerDTO)
+                .map(customer -> getCustomerDTOWithUrl(customer)
+                )
                 .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO findById(Long id) {
         Optional<Customer> foundCustomerOptional = customerRepository.findById(id);
-        return foundCustomerOptional.map(customerMapper::customerToCustomerDTO).orElse(null);
+        return foundCustomerOptional.map(customer -> getCustomerDTOWithUrl(customer)).orElse(null);
     }
 
     @Override
     public List<CustomerDTO> findByFirstName(String firstName) {
         return customerRepository.findByFirstName(firstName)
                 .stream()
-                .map(customerMapper::customerToCustomerDTO)
+                .map(customer -> getCustomerDTOWithUrl(customer))
                 .collect(Collectors.toList());
+    }
+
+    private CustomerDTO getCustomerDTOWithUrl(Customer customer) {
+        CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+        customerDTO.setCustomerURL(API_V_1_CUSTOMERS +customer.getId());
+        return customerDTO;
     }
 }
