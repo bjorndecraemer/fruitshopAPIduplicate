6e22feede6cadfe8682bf5e5341280cb55d97099
bjorn <bjorn.decraemer@yahoo.com>
2018-12-15 13:21:52 +0100

    Implemented CustomerServiceImpl

--- src/main/java/bjorn/petprojects/fruitshopduplicate/services/CustomerServiceImpl.java
+++ src/main/java/bjorn/petprojects/fruitshopduplicate/services/CustomerServiceImpl.java
@@ -1,30 +1,40 @@
 package bjorn.petprojects.fruitshopduplicate.services;
 
+import bjorn.petprojects.fruitshopduplicate.api.v1.mapper.CustomerMapper;
 import bjorn.petprojects.fruitshopduplicate.api.v1.model.CustomerDTO;
+import bjorn.petprojects.fruitshopduplicate.domain.Customer;
 import bjorn.petprojects.fruitshopduplicate.repository.CustomerRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+        this.customerMapper = CustomerMapper.INSTANCE;
     }
 
     @Override
     public List<CustomerDTO> findAllCustomers() {
-        return null;
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO findById(Long id) {
-        return null;
+        Optional<Customer> foundCustomerOptional = customerRepository.findById(id);
+        return foundCustomerOptional.map(customerMapper::customerToCustomerDTO).orElse(null);
     }
 
     @Override
     public CustomerDTO findByFirstName(String firstName) {
-        return null;
+        return customerMapper.customerToCustomerDTO(customerRepository.findByFirstName(firstName));
     }
 }
